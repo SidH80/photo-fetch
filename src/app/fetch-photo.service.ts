74129646a50fabd10a3bc78fc8a74abf3e308748
lethalSid <senien.hicks@gmail.com>
2020-08-19 00:30:47 -0400

    committing access key

--- src/app/fetch-photo.service.ts
+++ src/app/fetch-photo.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchPhotoService {
-
-  constructor() { }
+  accessKey = 'FRCy5qZBX910f0Sa8y_9Ajs7kv5tuFSiEzUXzL8P9KE'
+  constructor(private httpClient: HttpClientModule) { }
 }
